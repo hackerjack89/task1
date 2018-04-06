e51e6651e98d69629cb10aa4d2a475b3e85f14ba
prathamesh doiphode <prathamesh.doiphode@raweng.com>
2018-04-06 10:21:43 +0530

    made array adapter functions async

--- adapters/array.js
+++ adapters/array.js
@@ -1,54 +1,68 @@
+class ArrayAdapter {
+	constructor() {
+		this.collection = [];
+	}
 
-class ArrayAdapter{
-    constructor(){
-        this.collection=[];
-    }
+	getAll(cb) {
+		if (typeof cb === 'function') {
+			cb(this.collection);
+		}
+	}
 
-    getAll(){
-        return this.collection;
-    }
+	save(obj, cb) {
+		//get the value of id attribute from array length,
+		//since array index can be used as an id to locate
+		//an element in array
+		var _id = this.collection.length;
+		obj["_id"] = _id;
+		this.collection.push(obj)
+		if (typeof cb === 'function') {
+			cb(this.collection[_id])
+		}
+	}
+
+	updateWithQuery(query, changeset, cb) {
+		var keys = Object.keys(query);
+		var index = this.collection.findIndex(x => x[keys[0]] == query[keys[0]]);
+		if (index > -1) { //Element exists
+			changeset["_id"] = index;
+			this.collection[index] = changeset;
+			if (typeof cb === 'function') {
+				cb(this.collection[index]);
+			}
+		}
+	}
 
 
-    save(obj){
-        obj["_id"] = this.collection.length;
-        this.collection.push(obj)
-        return obj["_id"];
-    }
+	find(query, cb) {
+		var keys = Object.keys(query);
+		if(typeof cb === 'function'){
+			cb( this.collection.filter(x => x[keys[0]] == query[keys[0]]) )
+		}
+	}
 
-    updateWithQuery(query, changeset){
-        var keys = Object.keys(query);
-        var index = this.collection.findIndex(x => x[keys[0]] == query[keys[0]]);
-        if(index > -1){ //Element exists
-            changeset["_id"]=index;
-            this.collection[index]=changeset;
-            return this.collection[index];
-        }else{
-            return null
-        }
-    }
+	update(obj, cb) {
+		var new_data = this.collection[obj._id];
+		var keys = Object.keys(obj)
+		keys.forEach(k => {
+			new_data[k] = obj[k]
+		});
+		this.collection[obj._id] = new_data;
+		if(typeof cb === 'function'){
+			cb(this.collection[obj._id])
+		}
+	}
 
-    find(query){
-        var keys = Object.keys(query);
-        return this.collection.filter(x => x[keys[0]] == query[keys[0]])
-    }
+	findOne(index, cb) {
+		if (typeof cb === 'function') {
+			if (this.collection[index] != undefined) {
+				cb(this.collection[index]);
+			} else {
+				cb(null);
+			}
+		}
+	}
 
-    update(obj) {
-        this.collection[obj._id] = obj
-        return obj;
-    }
-
-    delete(index){
-        console.log("delete")
-    }
-
-    findOne(index){
-        
-        if(this.collection[index] != undefined){
-            return this.collection[index];
-        }else{
-            return null;
-        }
-    }
 }
 
 
---
+++ person.js
@@ -0,0 +1,86 @@
+var ArrayAdapter = require('./adapters/array');
+var arrayAdapter = new ArrayAdapter();
+
+class Person {
+  constructor(obj={}, adapter){
+   this.data={}
+   this.adapter = adapter || arrayAdapter;
+   this.delta = obj;
+  }
+
+  toJSON() {
+    return this.data
+  }
+
+  set(key, value){
+    this.delta["_id"]= this.data._id;
+    this.delta[key] = value;
+    return this;
+  }
+
+  assign(obj){
+    this.delta["_id"]=this.data._id;
+    for(var i in obj){
+      this.delta[i] = obj[i];
+    }
+    return this;
+  }
+
+  revert(){
+    this.delta={}
+    return this;
+  }
+
+  save() {
+    this.adapter.save(this.delta, (savedObj) => {
+      this.delta = {};
+      this.data = savedObj;
+    });
+    return this;
+  }
+
+  update(cb) {
+    this.adapter.update(this.delta, (res) => {
+      this.data = res;
+      this.delta = {}
+      if (typeof cb === 'function') {
+        cb(res);
+      }
+    });
+  }
+
+  static updateWithQuery(query,changeset,cb, adapter=arrayAdapter){
+    adapter.updateWithQuery(query, changeset, (res) => {
+      if(typeof cb === 'function'){
+        return cb(res);
+      }
+    });
+  }
+
+  static findOne(index, cb, adapter=arrayAdapter){
+    adapter.findOne(index, (res) => {
+      if(typeof cb === 'function'){
+        cb(res);
+      }
+    });
+  }
+
+  static find(query, cb, adapter=arrayAdapter){
+    adapter.find(query,(res) => {
+      if(typeof cb === 'function'){
+        cb(res)
+      }
+    })
+    
+  }
+
+   static getAll(cb, adapter=arrayAdapter){
+     adapter.getAll( (people) => {
+       if(typeof cb === 'function'){
+        return cb(people);
+       }
+    });
+  }
+}
+
+module.exports = Person;
